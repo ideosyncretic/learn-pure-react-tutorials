9f03abfa2da4e26840646de31c251b64f79041c6
Sprazzeus <sprazzeus@gmail.com>
2016-11-02 16:52:16 +0800

    added FileList component and testFiles data

--- github-example/src/index.js
+++ github-example/src/index.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
+class FileList extends Component {
+  render () {
+    let files = this.props.files
+    return (
+      <table className='file-list'>
+        <tbody>
+          {files.map(file => (
+            <tr className='file-list-item' key={file.id}>
+              <td className='file-name'>{file.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
+  }
+}
+FileList.propTypes = {
+  files: React.PropTypes.array
+}
+
+const testFiles = [
+  {
+    id: 1,
+    name: 'src',
+    type: 'folder',
+    updated_at: '2016-07-11 21:24:00',
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  },
+  {
+    id: 2,
+    name: 'tests',
+    type: 'folder',
+    updated_at: '2016-07-11 21:24:00',
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  },
+  {
+    id: 3,
+    name: 'README',
+    type: 'file',
+    updated_at: '2016-07-18 21:24:00',
+    latestCommit: {
+      message: 'Added a readme'
+    }
+  }
+]
+
 ReactDOM.render(
-  <App />,
+  <FileList files={testFiles} />,
   document.getElementById('root')
 );
